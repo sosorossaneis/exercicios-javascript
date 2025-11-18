d585808a5806b50a01cc4f0aae205892aca80e85
SOPHIA ROSSANEIS BARBOSA <55504676827@imaccommand007.local>
2025-11-18 14:01:56 -0300

    exercicio 01

---
+++ exemplo.js
@@ -0,0 +1,24 @@
+//Alert - Apresenta uma mensagem na tela//
+window.alert("Ola turma do Borges")
+//Confirm - Pergunta com o ok / cancela (Retorna um True ou False)//
+window.confirm("O Jonas vai casar")
+//Prompt - Pega  o texto da tela (Pergunta algo)//
+// window.prompt("Qual o seu nome?")
+
+// console.log("Multimidia")
+
+// let nome = prompt("Informe o seu nome: ")
+
+// console.log("Bem Vindo!" + nome)
+
+// let numero1 = Number.parseInt(prompt("Digite o primeiro número:  "))
+// let numero2 = Number.parseInt(prompt("Digite o segundo número:  "))
+
+// let resultado = numero1 + numero2 
+
+let numero1 = Number.parseFloat(prompt("Digite o primeiro número:  "))
+let numero2 = Number.parseFloat(prompt("Digite o segundo número:  "))
+
+let resultado = numero1 + numero2 
+
+console.log(resultado)
---
+++ exercicio01.js
@@ -0,0 +1,16 @@
+// Exercício 1: Verificação de login
+// Escreva um programa que solicita ao usuário um nome de usuário 
+// e uma senha.Verifique se o nome de usuário é "admin" e a 
+// senha é "senha123".Exiba uma mensagem indicando se o login
+// foi bem - sucedido ou não.
+
+let login = prompt("Digite o seu usuário: ")
+
+let senha = prompt("Digite a sua senha: ")
+
+if(login == "admin" && senha == "senha123"){
+    console.log("Login bem sucedido!")
+}
+else{
+    console.log("Você não tem permissão de acesso!")
+}
