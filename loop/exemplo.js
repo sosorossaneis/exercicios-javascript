04546a67c7c74200f2cd042412dcb6d53da0e4fa
SOPHIA ROSSANEIS BARBOSA <55504676827@imaccommand007.local>
2025-11-25 09:16:20 -0300

    Loop

---
+++ loop/exemplo.js
@@ -0,0 +1,51 @@
+//for - loop contado
+
+//contador do 0 ao 10
+
+// for (inicializador; condição; contador)
+
+// for (let num = 0; num <= 10; num++) {
+//     console.log("Eu vim barganhar " + num)
+// }
+
+//while - loop condicional
+
+// let salario = 1000
+
+// while (salario < 5000) {
+//     // salario = 100 + salario
+//     salario += 100
+
+//     console.log("O salário é de R$: " + salario)
+// }
+
+// //Do While - Loop condicional
+
+// let aumento = 1000
+
+// do {
+//     console.log("Esse é o seu saldo: " + aumento)
+//     aumento += 100
+
+// } while (aumento < 5000)
+
+//Array - 
+
+let frutas = ["Banana", "Laranja", "Maçã", "Pitaya", "Melancia", "Uva"]
+
+// console.log(fruta[3])
+// console.log(fruta[5])
+
+//forEach => percorre uma lista
+frutas.forEach(fruta => {
+    console.log("A fruta é: " + fruta)
+})
+
+//Percorrer uma lista com for
+for  (let indice = 0; indice < frutas.length; indice++){
+    console.log(frutas[indice])
+    
+    if(frutas[indice] == "Uva"){
+        console.log("Eu gosto de " + frutas[indice])
+    }
+}
