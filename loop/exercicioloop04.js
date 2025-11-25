eb1339d1ef38bf6a8ee92130ebd6b0a9295bb236
SOPHIA ROSSANEIS BARBOSA <55504676827@imaccommand007.local>
2025-11-25 11:09:16 -0300

    exercicio 04 - loop impar

---
+++ loop/exercicioloop04.js
@@ -0,0 +1,16 @@
+//4. Faça um programa que imprima os números impares de 0 a 50
+
+
+for(let num = 0; num <=50; num++){
+    if(num%2 !==0){
+        console.log(num)
+    }
+}
+
+//!== = Diferente
+
+console.log("-----------------------------------")
+
+for(let numero = 1; numero<=50; numero += 2){
+    console.log(numero)
+}
---
+++ loop/exerciciowhile06.js
@@ -0,0 +1,8 @@
+// 6. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+
+let num = 0 
+while(num <= 100){
+    num = 1 + num
+    num += 100
+}
